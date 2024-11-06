65a8154ee38054f794f24dbf41594219189f6f3f
Calvin Ye <you@example.com>
2024-11-06 15:33:22 -0500

    Added clarification comment

--- attendance.py
+++ attendance.py
@@ -3,7 +3,7 @@ import csv
 file = csv.reader(open('attendance.csv', 'r')) # Change attendance.csv to name of attendance file
 
 def attendanceRate(osis):
-    string = str(osis)
+    string = str(osis) # Failsafe in case osis is entered as an int
     for row in file:
         if row[0] == string:
             return row[2]
