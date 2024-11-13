9a2b4c82a2d94d473ae211a0d62f57ab9d5a118c
Daniel Yang <danielwzyang@gmail.com>
2024-11-13 17:26:42 -0500

    fixed error with wrong ordering of user and all meetings

--- src/pages/api/fetch/meetings.ts
+++ src/pages/api/fetch/meetings.ts
@@ -7,13 +7,21 @@ export const GET: APIRoute = async ({ request }) => {
     const osis = new URL(request.url).searchParams.get("osis")
     // fetches the meetings that the user attended and all the meetings
     // data should be an array; the first element is the user's meetings, second is all the meetings
-    const { data, error } = await supabase
+    const { data: userMeetings, error: error1 } = await supabase
         .from("meetings")
         .select("meetings")
-        .in("id", [osis, "all"])
+        .eq("id", osis)
 
-    // handles any errors
-    if (error) return new Response("Error while fetching user data", { status: 400 })
-        
-    return new Response(JSON.stringify(data.map(e => e.meetings)))
+    if (error1) return new Response(error1.message, { status: 500 })
+
+    const { data: allMeetings, error: error2 } = await supabase
+        .from("meetings")
+        .select("meetings")
+        .eq("id", "all")
+
+    if (error2) return new Response(error2.message, { status: 500 })
+    
+    const data = [userMeetings[0].meetings, allMeetings[0].meetings]
+
+    return new Response(JSON.stringify(data))
 }
